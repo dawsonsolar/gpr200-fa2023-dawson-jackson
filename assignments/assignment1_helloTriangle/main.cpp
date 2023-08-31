83f5b3795ce0d717038841a1c43fafe5a80b0848
Dawson Jackson <dawson.jackson@mymail.champlain.edu>
2023-08-31 15:45:16 -0400

    general setup and tiny start on hello triangle.

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -27,6 +27,32 @@ int main() {
 		return 1;
 	}
 
+
+	float vertices[9] = {
+		//x   //y  //z
+		-0.5, -0.5, 0.0, //Bottom left
+		 0.5, -0.5, 0.0, //Bottom right
+		 0.0,  0.5, 0.0  //Top center
+	};
+
+
+	//Define a new buffer id
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//Allocate space for + send vertex data to GPU.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int vao;
+	glGenVertexArrays(vao);
+	glBindVertexArray(vao);
+	//Tell vao to pull vertex data from vbo
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	//Define position attribute (3 floats)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	glEnableVertexAttribArray(0);
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
